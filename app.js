b13f4a9d30109e48ed29e9a9168730dcfadef51a
unknown <you@example.com>
2021-10-03 19:27:22 +0300

    DZ-1

---
+++ app.js
@@ -0,0 +1,29 @@
+let a = "green";
+let b = "red";
+console.log(a);
+console.log(b);
+b = "green";
+a = "red";
+console.log(a);
+console.log(b);
+
+const numbers = [2, 6, 7, 1, 3, 5, 0, 4, 8, 9];
+const sum = (numbers[1] + numbers[4] + numbers[3]);
+console.log(sum);
+
+function first() {
+    let a = "green";
+    let b = "red";
+    const numbers = [2, 6, 7, 1, 3, 5, 0, 4, 8, 9];
+    const sum = (numbers[1] + numbers[4] + numbers[3]);    
+        const subjects = {
+            a: "green",
+            b: "red",
+            sum: 10            
+        }
+    console.log(subjects);
+}
+first();
+
+
+
